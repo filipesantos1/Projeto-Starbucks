9a934f3ec097c9cf8f3272b8310147e17c7f560d
filipesantos1 <filipedsantos78@gmail.com>
2025-04-15 11:09:21 -0300

    Primeiro Commit - Projeto Starbuks

---
+++ script.js
@@ -0,0 +1,15 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+
+function trocaImagem(endereco) {
+imagem.src = endereco
+
+}
+
+function trocaCor(route) {
+   circulo.style.background  = route
+
+   
+}
+
